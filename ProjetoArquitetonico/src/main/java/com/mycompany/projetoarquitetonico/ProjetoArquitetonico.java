dc31c6849440661197ff937c7be71b071de10e5c
Yuri Tainã <yuritaina.232@gmail.com>
2024-11-10 15:02:37 -0300

    Forms

---
+++ ProjetoArquitetonico/src/main/java/com/mycompany/projetoarquitetonico/ProjetoArquitetonico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoarquitetonico;
+
+/**
+ *
+ * @author yurit
+ */
+public class ProjetoArquitetonico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
