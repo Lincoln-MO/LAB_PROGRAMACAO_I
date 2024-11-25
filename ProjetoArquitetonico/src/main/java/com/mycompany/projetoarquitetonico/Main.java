0a17da5ca46f06e09cba2adcc56ff3a507d392df
Lincoln Martins de Oliveira <120598136+Lincoln-MO@users.noreply.github.com>
2024-11-24 23:40:30 -0300

    Add files via upload

--- ProjetoArquitetonico/src/main/java/com/mycompany/projetoarquitetonico/Main.java
+++ ProjetoArquitetonico/src/main/java/com/mycompany/projetoarquitetonico/Main.java
@@ -10,17 +10,14 @@ package com.mycompany.projetoarquitetonico;
  * @author lincoln
  */
 
-import com.mycompany.projetoarquitetonico.forms.frmMain;
-import com.mycompany.projetoarquitetonico.Controllers.LoginController;
-import com.mycompany.projetoarquitetonico.forms.frmClientRegistration;
-import com.mycompany.projetoarquitetonico.Controllers.ClientRegistrationController;
-import com.mycompany.projetoarquitetonico.forms.frmEngineer;
-import com.mycompany.projetoarquitetonico.Controllers.EngineerController;
+import com.mycompany.projetoarquitetonico.forms.*;
+import com.mycompany.projetoarquitetonico.Controllers.*;
+
 
 public class Main {
     public static void main(String[] args) {
         frmMain mainView = new frmMain();
-        LoginController loginController = new LoginController(mainView);
+        LoginController loginController = new LoginController(mainView);  // Conecta a view ao controlador
         mainView.setVisible(true);
         
         java.awt.EventQueue.invokeLater(() -> {
@@ -30,8 +27,16 @@ public class Main {
         });
         java.awt.EventQueue.invokeLater(() -> {
             frmEngineer engineerView = new frmEngineer();
-            new EngineerController(engineerView);
+            new EngineerController(engineerView); // Conecta a view ao controlador
             engineerView.setVisible(true);
+        });
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                frmAdmin adminView = new frmAdmin();
+                new AdminController(adminView); // Conecta a view ao controlador
+                adminView.setVisible(true);
+            }
         });
     }
 }
